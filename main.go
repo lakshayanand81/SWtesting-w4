c9bfd96df534168397d940202d69ec5a34fc2da6
Nikasha <nikashamishra4@gmail.com>
2023-09-28 20:38:06 -0400

    Nikasha

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import "fmt" 
+       "strings"
+
 
 func addLetters() {
 	letter1 := "A"
@@ -8,6 +10,33 @@ func addLetters() {
 	result := letter1 + letter2
 	fmt.Printf("The result of adding letters %s and %s is: %s\n", letter1, letter2, result)
 }
+
+func pallindrome() {
+	var input string
+
+	fmt.Print("Enter a string: ")
+	fmt.Scanln(&input)
+
+	// Convert the string to lowercase and remove spaces
+	input = strings.ToLower(input)
+	input = strings.ReplaceAll(input, " ", "")
+
+	// Compare the string with its reverse
+	isPalindrome := true
+	for i := 0; i < len(input)/2; i++ {
+		if input[i] != input[len(input)-1-i] {
+			isPalindrome = false
+			break
+		}
+	}
+
+	if isPalindrome {
+		fmt.Println("The input is a palindrome.")
+	} else {
+		fmt.Println("The input is not a palindrome.")
+	}
+}
+
 
 func main() {
 	// Initialize a variable to store the highest number
